a9df226a56a21410d73495a62a6eeba117725eaf
Julio <juliobianco@gmail.com>
2013-10-22 22:48:38 -0300

    Adding YanApplication

---
+++ src/com/shinetech/android/YanApplication.java
@@ -0,0 +1,28 @@
+package com.shinetech.android;
+
+import android.app.Application;
+import android.util.Log;
+
+public class YanApplication extends Application {
+	public static final String PREFS_NAME = "YanPreferences";
+	private static final String TAG = "YanApplication";
+
+	private Preferences preferences = null;
+
+	@Override
+	public void onCreate() {
+		Log.i(TAG, "onCreate()");
+		super.onCreate();
+		preferences = new Preferences(getSharedPreferences(PREFS_NAME, 0));
+		preferences.load();
+	}
+
+	public Preferences getPreferences() {
+		return preferences;
+	}
+
+	public void setPreferences(Preferences preferences) {
+		this.preferences = preferences;
+	}
+	
+}
